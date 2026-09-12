478a695249ccc7af9285fa30bd380ab9807aa750
Matias <matiasgerstner@gmail.com>
2026-09-12 09:38:56 -0300

    style(crm): polish workspace ux

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app/App';
 import './styles.css';
+import './crm-polish.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>);
